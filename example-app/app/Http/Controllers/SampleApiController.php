1919638fe631dd1c296f33e032c41aa2cd94cf9d
松野浩士 <amanojac52@gmail.com>
2021-01-14 21:00:28 +0900

    オブジェクトを入れてみるテスト

--- example-app/app/Http/Controllers/SampleApiController.php
+++ example-app/app/Http/Controllers/SampleApiController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class SampleApiController extends Controller
 {
     public function sample()
     {
-        $content = [
-            'message' => 'はろーわーるど'
+        $users = User::all();
+        $data = [
+            'message' => 'success',
+            'users' =>  $users,
         ];
-        return response()->json(data: $content, options: JSON_UNESCAPED_UNICODE);
+        return response()->json(data: $data, options: JSON_UNESCAPED_UNICODE);
     }
 }
